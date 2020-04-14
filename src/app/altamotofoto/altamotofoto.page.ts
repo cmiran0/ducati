0fef50f3f7a1418523159cc050d853397e0051f0
cmiran0 <cmirand0@jaumebalmes.net>
2020-04-14 18:39:43 +0200

    1.0

--- src/app/altamotofoto/altamotofoto.page.ts
+++ src/app/altamotofoto/altamotofoto.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-altamotofoto',
@@ -9,16 +10,35 @@ export class AltamotofotoPage implements OnInit {
     marca: string;
     modelo: string;
     any: string;
-    foto: string;
-    precio: number;
+    precio: string;
+    image;
 
-    constructor() {
-    }
+    form = new FormData;
+
+    constructor(private router: Router) { }
 
     ngOnInit() {
     }
 
-    async addBike() {
+    async generarForm() {
+        this.image = (document.getElementsByName('foto')[0] as HTMLInputElement).files[0];
+        this.form.append('foto', this.image);
+        this.form.append('marca', this.marca);
+        this.form.append('modelo', this.modelo);
+        this.form.append('ano', this.any);
+        this.form.append('precio', this.precio);
 
+    }
+
+    async addBike() {
+        this.generarForm();
+        fetch('https://motos.puigverd.org/moto/foto', {
+            method: 'POST',
+            body: this.form
+        })
+            .then(response => {
+                console.log(response);
+                this.router.navigate(['home']);
+            });
     }
 }
